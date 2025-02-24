286530dc4ed0bd7c2ee88a3965a0fee8bcbb8404
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2025-02-24 17:17:57 -0300

    make simple tests on python

---
+++ solid/test02.py
@@ -0,0 +1,63 @@
+class Vehicle:
+    def __init__(self, typeVehicle: str, name: str, model: str, horses: float, quantyWheels: int):
+        self.typeVehicle: str = typeVehicle
+        self.model: str = model
+        self.name: str = name
+        self.horses: float = horses
+        self.quantyWheels: int = quantyWheels
+        self.isOn: bool = False
+
+    def getTypeVehicle(self) -> str:
+        return self.typeVehicle
+    
+    def getModel(self) -> str:
+        return self.model
+    
+    def getName(self) -> str:
+        return self.name
+    
+    def getHorses(self) -> float:
+        return self.horses
+    
+    def getQuantityWheels(self) -> int:
+        return self.quantyWheels
+    
+    def getIsOn(self) -> bool:
+        return self.isOn
+
+    def start(self) -> None:
+        if not self.isOn:
+            print(f"Ligando o(a) {self.typeVehicle}") 
+        else: 
+            print("Veiculo já esta ligado!")
+
+    def off(self) -> None:
+        if self.isOn:
+            print(f"Desligando o(a) {self.typeVehicle}") 
+        else: 
+            print("Veiculo já esta desligado!")
+
+    def maxVelocity(self) -> None:
+        print(f"Velocidade maxima {(self.horses / 60) * 25}")
+
+    def makeSound() -> None:
+        NotImplementedError("Implemente o metodo!")
+
+class Car(Vehicle):
+    def __init__(self, typeVehicle, name, model, horses, quantyWheels, quantityDoors: int):
+        super().__init__(typeVehicle, name, model, horses, quantyWheels)
+
+        self.quantyDoors: int = quantityDoors
+
+    def makeSound():
+        print("Vrum Vrum! tututututu!")
+
+class Bike(Vehicle):
+    def __init__(self, typeVehicle, name, model, horses, quantyWheels):
+        super().__init__(typeVehicle, name, model, horses, quantyWheels)
+
+    def makeSound() -> None:
+        print("randaandandandan!")
+
+if __name__ == "__main__":
+    motocycle: Bike = Bike("Moto")
