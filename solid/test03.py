57394d4f1113d283182af977ba0f0e451d70cd7d
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-15 09:39:23 -0300

    feat: making my tests

--- solid/test03.py
+++ solid/test03.py
@@ -1,5 +1,6 @@
 def find_sum(find: float | int, list_: list[float | int]) -> dict:
-    assert isinstance(find, float | int), "Invalido o valor 'find' deve ser flutuante ou inteiro" 
+    assert isinstance(find, float | int), "Invalido o valor 'find' deve ser flutuante ou inteiro"
+    assert isinstance(list_, list), "Invalido o valor 'list_' deve ser uma lista com números flutuantes ou inteiros"
 
     list_.sort()
     sum_: float | int
@@ -7,6 +8,6 @@ def find_sum(find: float | int, list_: list[float | int]) -> dict:
         for v in list_:
             sum_ = v + index
             if sum_ == find: return f"valor encontrado {v} + {index} = {v + index}"
-            
+
 
 print(find_sum(18, [4, 11, 2, 8, 9]))
